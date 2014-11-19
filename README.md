fea83a93c820c3d2be43747376e97dd242e02b40
Rob Blackwell <rob.blackwell@robblackwell.org.uk>
2014-11-19 13:44:23 +0000

    Updated for latest braid

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 braid-drakma
 ============
 
-An adapter that allows Common Lisp Braid middleware to be used with the Drakma HTTP client.
+An adapter that allows Common Lisp Braid middleware to be used with
+the Drakma HTTP client.
 
 Rob Blackwell   
 February 2013
--- braid-drakma.lisp
+++ braid-drakma.lisp
@@ -17,20 +17,26 @@
 (defmethod drakma-certificate-password ((request cons))
   (getf request :drakma-certificate-password))
 
+(defmethod drakma-force-binary ((request cons))
+  (getf request :drakma-force-binary))
+
+;; TODO: Consider supporting wider drakma options such as proxy etc.
+
 ;;;
 
-(defun web-request (request)
-  "Uses Drakma to make the specificed http request."
-  (multiple-value-bind  (body status-code headers)
+(defun web-request (request &key (force-binary nil))
+  "Uses Drakma to make the specified http request."
+  (multiple-value-bind  (body status headers)
       (drakma:http-request (request-uri request) 
 			   :method (request-method request) 
 			   :content-type nil
-			   :additional-headers (message-headers request) 
-			   :content (message-body request)
+			   :additional-headers (plist-alist (headers request))
+			   :content (body request)
 			   :certificate (drakma-certificate request)
 			   :key (drakma-key request)
-			   :certificate-password (drakma-certificate-password request))
-    (braid:make-response :status-code status-code :headers headers :body body)))
+			   :certificate-password (drakma-certificate-password request)
+				 :force-binary (drakma-force-binary request))
+    (braid:make-response :status status :headers headers :body body)))
 
 
 
