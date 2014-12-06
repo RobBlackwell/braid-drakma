1bc76ef59776d40e51dbce2be9a627c8d511b554
Rob Blackwell <rob.blackwell@robblackwell.org.uk>
2014-12-06 17:20:00 +0000

    Major changes

--- braid-drakma.lisp
+++ braid-drakma.lisp
@@ -4,39 +4,39 @@
 
 ;;; Extend the braid interface with Drakma specific options
 
-(defgeneric drakma-certificate (request))
-(defgeneric drakma-key (request))
-(defgeneric drakma-certificate-password (request))
+;; (defgeneric drakma-certificate (request))
+;; (defgeneric drakma-key (request))
+;; (defgeneric drakma-certificate-password (request))
 
-(defmethod drakma-certificate ((request cons))
-  (getf request :drakma-certificate))
+;; (defmethod drakma-certificate ((request cons))
+;;   (getf request :drakma-certificate))
 
-(defmethod drakma-key ((request cons))
-  (getf request :drakma-key))
+;; (defmethod drakma-key ((request cons))
+;;   (getf request :drakma-key))
 
-(defmethod drakma-certificate-password ((request cons))
-  (getf request :drakma-certificate-password))
+;; (defmethod drakma-certificate-password ((request cons))
+;;   (getf request :drakma-certificate-password))
 
-(defmethod drakma-force-binary ((request cons))
-  (getf request :drakma-force-binary))
+;; (defmethod drakma-force-binary ((request cons))
+;;   (getf request :drakma-force-binary))
 
 ;; TODO: Consider supporting wider drakma options such as proxy etc.
 
 ;;;
 
-(defun web-request (request)
+(defun web-request (http-request &key (drakma-key nil) (drakma-certificate nil) (drakma-certificate-password nil)(drakma-force-binary nil))
   "Uses Drakma to make the specified http request."
   (multiple-value-bind  (body status headers)
-      (drakma:http-request (uri request) 
-			   :method (request-method request) 
-			   :content-type nil
-			   :additional-headers (plist-alist (headers request))
-			   :content (body request)
-			   :certificate (drakma-certificate request)
-			   :key (drakma-key request)
-			   :certificate-password (drakma-certificate-password request)
-				 :force-binary (drakma-force-binary request))
-    (braid:make-response :status status :headers (alexandria:alist-plist headers) :body body)))
+      (drakma:http-request (http-request-uri http-request) 
+						   :method (http-request-method http-request) 
+						   :content-type nil
+						   :additional-headers (plist-alist (http-request-headers http-request))
+						   :content (http-request-body http-request)
+						   :certificate drakma-certificate
+						   :key drakma-key
+						   :certificate-password drakma-certificate-password
+						   :force-binary drakma-force-binary)
+    (braid:make-http-response :status status :headers (alexandria:alist-plist headers) :body body)))
 
 ;;; End
 
